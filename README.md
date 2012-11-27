6257e2a228e292dba436ce5023100633a569e673
Daniel Hardman <dhardman@adaptivecomputing.com>
2012-11-27 14:59:42 -0700

    first checkin

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 accap
-=====
+=======
+
+Capacity planner for Adaptive Computing's Moab Cloud Suite or Moab HPC Suite.
---
+++ code/accap.py
@@ -0,0 +1,6 @@
+from textui.ansi import *
+from textui.colors import *
+from textui.getch import *
+
+if __name__ == '__main__':
+    print 'to do'
