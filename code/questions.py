d027130366d0b3298a7aa687ec67ece1dba3022d
dhh1128 <dhardman@adaptivecomputing.com>
2012-12-03 18:45:12 -0700

    add a second question; extract plumbing from q methods

--- code/questions.py
+++ code/questions.py
@@ -6,14 +6,26 @@ from textui.prompt import *
 def _strip_and_lower_case(txt):
     return txt.strip().lower()
 
-def which_mode(answers):
-    key = 'type of deployment'
-    dflt = 'cloud'
-    answer = prompt('Type of deployment', 'cloud|hpc', 
-                    default=answers.get(key, dflt),
-                    normfunc=_strip_and_lower_case,
-                    acceptfunc=lambda x: re.match('cloud|hpc', x))
+def fancy_prompt(q, regex, answers, dflt=None, normfunc=None, acceptfunc=None, comment=None):
+    key = q.strip().lower()
+    if not normfunc:
+        normfunc = _strip_and_lower_case
+    if (not acceptfunc) and regex:
+        acceptfunc = lambda val: re.match(regex, val)
+    if comment:
+        print(comment)
+        print('')
+    answer = prompt(q, regex, default=answers.get(key, dflt),
+                    normfunc=normfunc, acceptfunc=acceptfunc)
     answers[key] = answer
+    return answer
+
+def which_mode(answers):
+    answer = fancy_prompt('Type of deployment', 'cloud|hpc', answers)
+    return num_nodes
+    
+def num_nodes(answers):
+    answer = fancy_prompt('Number of nodes', '[1-9][0-9]{0,4}', answers)
     
 first = which_mode    
     
